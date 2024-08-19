9be619d7deb3dae85455a97e0f74497eb04c3c3e
wenbingzuo <jaly201319@hotmail.com>
2024-08-19 13:35:47 +0800

    chore: 1.1.3

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "13e1ec55"
-#define MARS_PATH "release/android/2024-t1"
+#define MARS_REVISION "dabf9b19"
+#define MARS_PATH "feature/shanbay"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-02-28 11:22:06"
+#define MARS_BUILD_TIME "2024-08-19 13:33:33"
 #define MARS_TAG ""
+#define MARS_BUILD_TIMESTAMP 1724045613
 
 #endif
